a40628d28d93db4ed60a6e5f1cdd5468d7668c33
Richard DZH <richarddzh@163.com>
2017-06-13 08:12:00 +0800

    Coding for render env

--- mathtex/htmlrender.py
+++ mathtex/htmlrender.py
@@ -1,5 +1,15 @@
 from mathtex.astree import MathTexAST
 from mathtex.fontmetric import FONT_METRICS
+from mathtex.util import Array2D
+from typing import List
+
+class RowColumnMeta:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.width = 0
+        self.height = 0
+        self.baseline = 0
 
 
 class HtmlElement:
@@ -20,6 +30,8 @@ class HtmlRender:
     def __init__(self):
         self.html_elements = []
         self.char_margin = 0
+        self.cell_margin = 0.5
+        self.line_margin = 0
         self.line_height = FONT_METRICS.height
 
     def render(self, node: MathTexAST, font_size) -> HtmlElement:
@@ -49,7 +61,7 @@ class HtmlRender:
             elem.children.append(child)
         return elem
 
-    def render_block(self, children, font_size):
+    def render_block(self, children, font_size) -> HtmlElement:
         elem = HtmlElement()
         elem.width = 0
         elem.height = 0
@@ -69,3 +81,28 @@ class HtmlRender:
         for child in elem.children:
             child.y = elem.baseline - child.baseline
         return elem
+
+    def render_env(self, lines, font_size):
+        a2d = Array2D()  # type: Array2D
+        rows = []  # type: List[RowColumnMeta]
+        columns = []  # type: List[RowColumnMeta]
+        for i in range(0, len(lines)):
+            rows.append(RowColumnMeta())
+            if i > 0:
+                rows[i].y = rows[i-1].y + rows[i-1].height + self.line_margin * font_size
+            line = lines[i]  # type: MathTexAST
+            for j in range(0, len(line.children)):
+                if j >= len(columns):
+                    columns.append(RowColumnMeta())
+                cell = line.children[j]  # type: MathTexAST
+                elem = self.render_block(cell.children, font_size)
+                a2d.set(i, j, elem)
+                if elem.height >= rows[i].height:
+                    rows[i].height = elem.height
+                if elem.baseline >= rows[i].baseline:
+                    rows[i].baseline = elem.baseline
+                if elem.width >= columns[j].width:
+                    columns[j].width = elem.width
+        for j in range(1, len(columns)):
+            columns[j].x = columns[j-1].x + columns[j-1].width + self.cell_margin * font_size
+
--- mathtex/util.py
+++ mathtex/util.py
@@ -3,3 +3,28 @@ def last_index(a_list, func):
         if func(a_list[i]):
             return i
     return -1
+
+
+class Array2D:
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+        self.rows = []
+
+    def set(self, y, x, elem):
+        if y >= self.height:
+            self.height = y + 1
+        if x >= self.width:
+            self.width = x + 1
+        while len(self.rows) < self.height:
+            self.rows.append([])
+        while len(self.rows[y]) < self.width:
+            self.rows[y].append(None)
+        self.rows[y][x] = elem
+
+    def get(self, y, x):
+        if y >= len(self.rows):
+            return None
+        if x >= len(self.rows[y]):
+            return None
+        return self.rows[y][x]
