5ba9e1ca5af9dc4bef218eb3cd1fb25c83bf4905
Richard Dong <richarddzh@gmail.com>
2017-06-24 15:44:21 +0800

    Add command line arg support

--- demo/demo.py
+++ demo/demo.py
@@ -1,3 +1,11 @@
 from mathtex import markdown
+from sys import argv
 
-markdown.save_markdown_as_html("../test.md", "../test.html")
+input_file = "test.md"
+output_file = "test.html"
+
+if len(argv) > 1:
+    input_file = argv[1] + ".md"
+    output_file = argv[1] + ".html"
+
+markdown.save_markdown_as_html(input_file, output_file)
